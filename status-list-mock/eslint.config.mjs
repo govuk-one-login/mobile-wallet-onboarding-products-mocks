174bb60f5144f6d1bdd21933376d605aa0513988
Niki Stefanopoulou <niki.stefanopoulou@digital.cabinet-office.gov.uk>
2026-05-21 14:39:54 +0100

    refactor: remove array and spread from eslint config

--- status-list-mock/eslint.config.mjs
+++ status-list-mock/eslint.config.mjs
@@ -2,9 +2,9 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-export default tseslint.config([
+export default tseslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+  tseslint.configs.recommended,
   eslintConfigPrettier, // Disables ESLint rules that conflict with Prettier
   {
     rules: {
@@ -19,4 +19,4 @@ export default tseslint.config([
       ],
     },
   },
-]);
+);
